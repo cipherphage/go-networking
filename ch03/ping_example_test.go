7eede8aa6cde6232cdf8d62ad05135d16858d365
j3dev <cipherphage@users.noreply.github.com>
2023-10-03 13:58:45 -0400

    turned example pinger into test and added test command and expected output as a comment

---
+++ ch03/ping_example_test.go
@@ -0,0 +1,71 @@
+package ch03
+
+import (
+	"context"
+	"io"
+	"testing"
+	"time"
+)
+
+func TestExamplePinger(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	r, w := io.Pipe() // In lieu of net.Conn.
+	done := make(chan struct{})
+	resetTimer := make(chan time.Duration, 1)
+	resetTimer <- time.Second // Initial ping interval.
+
+	go func() {
+		Pinger(ctx, w, resetTimer)
+		close(done)
+	}()
+
+	receivePing := func(d time.Duration, r io.Reader) {
+		if d >= 0 {
+			t.Logf("resetting timer (%s)\n", d)
+			resetTimer <- d
+		}
+
+		now := time.Now()
+		buf := make([]byte, 1024)
+		n, err := r.Read(buf)
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		t.Logf("received %q (%s)\n", buf[:n], time.Since(now).Round(100*time.Millisecond))
+	}
+
+	for i, v := range []int64{0, 200, 300, 0, -1, -1, -1} {
+		t.Logf("Run %d:\n", i+1)
+		receivePing(time.Duration(v)*time.Millisecond, r)
+	}
+
+	cancel()
+	<-done // Ensures the pinger exits after cancelling the context.
+}
+
+// EXPECTED OUTPUT
+// go test -v -timeout 300s -race -bench=. ./ch03/ping.go ./ch03/ping_example_test.go
+// === RUN   TestExamplePinger
+//     ping_example_test.go:40: Run 1:
+//     ping_example_test.go:24: resetting timer (0s)
+//     ping_example_test.go:36: received "ping" (1s)
+//     ping_example_test.go:40: Run 2:
+//     ping_example_test.go:24: resetting timer (200ms)
+//     ping_example_test.go:36: received "ping" (200ms)
+//     ping_example_test.go:40: Run 3:
+//     ping_example_test.go:24: resetting timer (300ms)
+//     ping_example_test.go:36: received "ping" (300ms)
+//     ping_example_test.go:40: Run 4:
+//     ping_example_test.go:24: resetting timer (0s)
+//     ping_example_test.go:36: received "ping" (300ms)
+//     ping_example_test.go:40: Run 5:
+//     ping_example_test.go:36: received "ping" (300ms)
+//     ping_example_test.go:40: Run 6:
+//     ping_example_test.go:36: received "ping" (300ms)
+//     ping_example_test.go:40: Run 7:
+//     ping_example_test.go:36: received "ping" (300ms)
+// --- PASS: TestExamplePinger (2.72s)
+// PASS
+// ok      command-line-arguments  3.409s
