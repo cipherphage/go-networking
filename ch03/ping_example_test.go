786250442259fd9b638b6a578922059d3568e145
j3dev <cipherphage@users.noreply.github.com>
2023-10-03 16:12:51 -0400

    go test can test expected output against stdout so the previous refactor was not necessary

--- ch03/ping_example_test.go
+++ ch03/ping_example_test.go
@@ -2,12 +2,12 @@ package ch03
 
 import (
 	"context"
+	"fmt"
 	"io"
-	"testing"
 	"time"
 )
 
-func TestExamplePinger(t *testing.T) {
+func ExamplePinger() {
 	ctx, cancel := context.WithCancel(context.Background())
 	r, w := io.Pipe() // In lieu of net.Conn.
 	done := make(chan struct{})
@@ -21,7 +21,8 @@ func TestExamplePinger(t *testing.T) {
 
 	receivePing := func(d time.Duration, r io.Reader) {
 		if d >= 0 {
-			t.Logf("resetting timer (%s)\n", d)
+			fmt.Printf("resetting timer (%s)\n", d)
+			// t.Logf("resetting timer (%s)\n", d)
 			resetTimer <- d
 		}
 
@@ -30,19 +31,42 @@ func TestExamplePinger(t *testing.T) {
 		n, err := r.Read(buf)
 
 		if err != nil {
-			t.Error(err)
+			fmt.Println(err)
+			//t.Error(err)
 		}
 
-		t.Logf("received %q (%s)\n", buf[:n], time.Since(now).Round(100*time.Millisecond))
+		fmt.Printf("received %q (%s)\n", buf[:n], time.Since(now).Round(100*time.Millisecond))
+		// t.Logf("received %q (%s)\n", buf[:n], time.Since(now).Round(100*time.Millisecond))
 	}
 
 	for i, v := range []int64{0, 200, 300, 0, -1, -1, -1} {
-		t.Logf("Run %d:\n", i+1)
+		fmt.Printf("Run %d:\n", i+1)
+		// t.Logf("Run %d:\n", i+1)
 		receivePing(time.Duration(v)*time.Millisecond, r)
 	}
 
 	cancel()
 	<-done // Ensures the pinger exits after cancelling the context.
+
+	// Output:
+	// Run 1:
+	// resetting timer (0s)
+	// received "ping" (1s)
+	// Run 2:
+	// resetting timer (200ms)
+	// received "ping" (200ms)
+	// Run 3:
+	// resetting timer (300ms)
+	// received "ping" (300ms)
+	// Run 4:
+	// resetting timer (0s)
+	// received "ping" (300ms)
+	// Run 5:
+	// received "ping" (300ms)
+	// Run 6:
+	// received "ping" (300ms)
+	// Run 7:
+	// received "ping" (300ms)
 }
 
 // EXPECTED OUTPUT
