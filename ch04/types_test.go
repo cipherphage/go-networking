0256ca9770b9a71d04648abfc130cbdaa7d3a0f2
j3dev <cipherphage@users.noreply.github.com>
2023-10-04 18:59:32 -0400

    added types and types test

---
+++ ch04/types_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"net"
+	"reflect"
+	"testing"
+)
+
+func TestPayloads(t *testing.T) {
+	b1 := Binary("Clear is better than clever.")
+	b2 := Binary("Don't panic.")
+	s1 := String("Errors are values.")
+	payloads := []Payload{&b1, &s1, &b2}
+
+	listener, err := net.Listen("tcp", "127.0.0.1:")
+	HandleTestError(t, err, "fatal")
+
+	go func() {
+		conn, err := listener.Accept()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		defer conn.Close()
+
+		for _, p := range payloads {
+			_, err = p.WriteTo(conn)
+			if err != nil {
+				t.Error(err)
+				break
+			}
+		}
+	}()
+
+	conn, err := net.Dial("tcp", listener.Addr().String())
+	HandleTestError(t, err, "fatal")
+	defer conn.Close()
+
+	for i := 0; i < len(payloads); i++ {
+		actual, err := decode(conn)
+		HandleTestError(t, err, "fatal")
+
+		if expected := payloads[i]; !reflect.DeepEqual(expected, actual) {
+			t.Errorf("value mismatch: %v != %v", expected, actual)
+			continue
+		}
+
+		t.Logf("[%T] %[1]q", actual)
+	}
+}
+
+func TestMaxPayloadSize(t *testing.T) {
+	buf := new(bytes.Buffer)
+	err := buf.WriteByte(BinaryType)
+	HandleTestError(t, err, "fatal")
+
+	err = binary.Write(buf, binary.BigEndian, uint32(1<<30)) // 1 GB.
+	HandleTestError(t, err, "fatal")
+
+	var b Binary
+	_, err = b.ReadFrom(buf)
+	if err != ErrMaxPayloadSize {
+		t.Fatalf("expected ErrMaxPayloadSize; actual: %v", err)
+	}
+}
